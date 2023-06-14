f56841b8c9887b8280ee73c59b13616e8357be29
Andres Ontiveros <andres.ontiveros24@gmail.com>
2023-06-14 07:35:36 -0500

    [UPDATE] Social Canvas Project
    Social Canvas library added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CardsComponent } from './cards/cards.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    CardsComponent
+    CardsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -14,7 +14,7 @@ export class MainPageComponent {
 public ArregloTarjetas: Tarjeta[]=[]
   ngOnInit(): void{
     this.ArregloTarjetas = [
-      {titulo: "Grupo 1", subtitulo: "Descripcion Grupo 1", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUBFOORMT6ny-Bw-cmuMcD_gPNJl1HvVQPCQ&usqp=CAU"},
+      {titulo: "Grupo 1", subtitulo: "Descripcion Grupo 1", image: "https://i.blogs.es/1cfea2/whatsapp-grupos-portada/1366_2000.jpg"},
       {titulo: "Grupo 2", subtitulo: "Descripcion Grupo 2", image: "https://www.womgp.com/blog/wp-content/uploads/2021/03/Desarrollo-Web-Dibdata.jpg"},
       {titulo: "Grupo 3", subtitulo: "Descripcion Grupo 3", image: "https://servisoftcorp.com/wp-content/uploads/2019/02/aplicaciones-moviles-apps-servisoftcorp.com_.jpg"},
     ]
